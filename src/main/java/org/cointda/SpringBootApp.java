89c37646861b6a83df06a553810af6d798b46765
leoxxx <lifxue@gmail.com>
2022-06-14 22:57:37 +0800

    增加springboot支持

---
+++ src/main/java/org/cointda/SpringBootApp.java
@@ -0,0 +1,18 @@
+package org.cointda;
+
+import javafx.application.Application;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Application.launch(App.class, args);
+    }
+}
