7428bf4b291e80258f7b1700cfb0c7c1fb1da0fd
leoxxx <lifxue@gmail.com>
2022-06-21 16:31:48 +0800

    测试coinMarketCapConfig

--- src/main/java/org/cointda/InitDBCommanLineRunner.java
+++ src/main/java/org/cointda/InitDBCommanLineRunner.java
@@ -1,8 +1,10 @@
 package org.cointda;
 
 import lombok.extern.slf4j.Slf4j;
+import org.cointda.config.CoinMarketCapConfig;
 import org.cointda.entity.TradeInfo;
 import org.cointda.mapper.TradeInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -18,20 +20,24 @@ public class InitDBCommanLineRunner implements CommandLineRunner {
     @Resource
     private TradeInfoMapper tradeInfoMapper;
 
+    @Autowired
+    private CoinMarketCapConfig coinMarketCapConfig;
+
     @Override
     public void run(String... args) throws Exception {
         log.info("加载顺序1");
-        TradeInfo tradeInfo = new TradeInfo();
-        tradeInfo.setBaseId(1);
-        tradeInfo.setBaseNum("1");
-        tradeInfo.setTradeDate("aa");
-        tradeInfo.setBaseSymbol("sd");
-        tradeInfo.setPrice("123");
-        tradeInfo.setQuoteId(1);
-        tradeInfo.setQuoteNum("1");
-        tradeInfo.setQuoteSymbol("aa");
-        tradeInfo.setSaleOrBuy("1");
-        tradeInfoMapper.insert(tradeInfo);
+        log.info("CoinMarketCapConfig.apiKey = "+ coinMarketCapConfig.getApiKey());
+        //TradeInfo tradeInfo = new TradeInfo();
+        //tradeInfo.setBaseId(1);
+        //tradeInfo.setBaseNum("1");
+        //tradeInfo.setTradeDate("aa");
+        //tradeInfo.setBaseSymbol("sd");
+        //tradeInfo.setPrice("123");
+        //tradeInfo.setQuoteId(1);
+        //tradeInfo.setQuoteNum("1");
+        //tradeInfo.setQuoteSymbol("aa");
+        //tradeInfo.setSaleOrBuy("1");
+        //tradeInfoMapper.insert(tradeInfo);
         List<TradeInfo> list = tradeInfoMapper.selectList(null);
         if(list != null && !list.isEmpty()){
             log.info("list.size === "+list.size());
