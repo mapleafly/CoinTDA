374903ff205c05e4809553a2356655b09e8a92d0
leoxxx <lifxue@gmail.com>
2022-06-21 16:30:23 +0800

    增加coinmarketcap配置类，application.yml中增加参数

---
+++ src/main/java/org/cointda/config/CoinMarketCapConfig.java
@@ -0,0 +1,24 @@
+package org.cointda.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "coin-market-cap")
+public class CoinMarketCapConfig {
+    private String apiKey;
+    private String customHeader;
+    private String httpHeader;
+    private String coinMarketCapIDMap;
+    private String latestlistings;
+    private String latestQuotes;
+    //private String metadata;
+    //private String historicalListings;
+    //private String historicalQuotes;
+    //private String latestMarketPairs;
+    //private String latestOHLCV;
+    //private String historicalOHLCV;
+    //private String pricePerformanceStats;
+}
