c9ba03f7ca035ff7c9fb5e11f8ba331c62aa6ba0
leoxxx <lifxue@gmail.com>
2022-06-25 18:20:06 +0800

    创建表对应的mapper

---
+++ src/main/java/org/cointda/mapper/CoinMarketCapListingsMapper.java
@@ -0,0 +1,7 @@
+package org.cointda.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.cointda.entity.CoinMarketCapListings;
+
+public interface CoinMarketCapListingsMapper extends BaseMapper<CoinMarketCapListings> {
+}
---
+++ src/main/java/org/cointda/mapper/QuotesLatestMapper.java
@@ -0,0 +1,7 @@
+package org.cointda.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.cointda.entity.QuotesLatest;
+
+public interface QuotesLatestMapper extends BaseMapper<QuotesLatest> {
+}
